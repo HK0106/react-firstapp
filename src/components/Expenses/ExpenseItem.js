6433784fe916b7dd2a85aff3db08a415a610fad0
duongpham.it <duongit123>
2022-03-29 21:49:58 +0700

    commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,20 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+
+function ExpenseItem(props) {
+
+    return (
+        <Card className='expense-item'>
+            <div>
+                <ExpenseDate date={props.date}/>
+            </div>
+            <div>
+                <h2 className='expense-item__description'>{props.title}</h2>
+                <div className='expense-item__price'>$ {props.amount}</div>
+            </div>
+        </Card>
+    )
+}
+
+export default ExpenseItem;
